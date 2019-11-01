b8ad8f3602abfec22e96075cc0ab497232430fa0
paninidasgupta <panini.dasgupta@gmail.com>
2019-11-01 19:12:06 +0530

    updated

--- signal_smooth_filter.py
+++ signal_smooth_filter.py
@@ -2,72 +2,97 @@ import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
 from scipy.stats.distributions import chi2
+import math    
+
 ### spectra codes are based on Eric J Oliver MATLAB codes ######
 #****************************** power spectrum *************************##
-def spec(signal,sample_freq,window_hann,*args):
+def spec(signal,sample_freq,window_type,alpha,**kwargs):
+    
+    ##************************ signal must have even length.************##
+    ## window=0 'no window'
+    ## window=1 'Hamming window'
+    ## window=2 'Hanning window'
     
     N=len(signal)                          #     no of points N
     Dt=sample_freq*1                       # sample frequency /rate
     T=N*Dt;                    
     fmax=1/(2*Dt);                         #     Nyquist (maximum) frequency,
     Df =fmax/(N/2);                        #     frequency interval,
-    Nf=N/2+1;                              #     number of non-negative frequencies,
-   
-    #     frequency vector , 
-    #f=Df*[0:N/2,-N/2þ1:-1]’;   
+    Nf=N/2+1;                              #     number of non-negative frequencies,#     frequency vector , #f=Df*[0:N/2,-N/2þ1:-1]’; 
+    signal=signal-np.mean(signal)
     
-    if window_hann:
-        signal=signal-np.mean(signal)
-        w1=0.54-0.46*np.cos(2*np.pi*np.arange(N)/(N-1)) ## hann window weight
-        signal1=w1*signal  
+    if window_type==1:
+        w1=0.54-0.46*np.cos(2*np.pi*np.arange(N)/(N-1)) ## hamming window weight
+    elif window_type==2:
+        w1=0.5-0.5*np.cos(2*np.pi*np.arange(N)/(N-1)) ## hanning window weight
     else:
-        signal1=signal*1
-        
+        w1=1
+    
+    signal1=w1*signal
     temp_fft = Dt*sc.fftpack.fft(signal1)
     fftfreq = np.fft.fftfreq(N,Dt)         # daily data it is 1./365 ## monthly data 1./12 ## yearly data=1
     temp_psd = temp_fft*np.conj(temp_fft)*(2/T)
-    w = fftfreq[(fftfreq >= 0) |(fftfreq==-0.5)]
-    w[-1]=-1*w[-1]
-    p = temp_psd[(fftfreq >= 0) | (fftfreq==-0.5)]
+    f = fftfreq[(fftfreq >= 0) |(fftfreq==-fmax)]
+    f[-1]=-1*f[-1]
+    p = temp_psd[(fftfreq >= 0) | (fftfreq==-fmax)]
     
     #Null Hypothesis: time series is uncorrelated random noise
     sd2est=np.std(signal);                   #   variance of time series,
-    ff=np.sum(w*w)/N;                        #   power in window function, 
+    ff=np.sum(w1*w1)/N;                        #   power in window function, 
     c = (ff*sd2est)/(2*Nf*Df);               #   scaling constant,  
-    cl95 = c*chi2.ppf(0.95, df=2);           #   95% confidence level,
+    cl95 = c*chi2.ppf(1.0-alpha, df=2);           #   95% confidence level,
     
-    if len(args)!=0:
-        ax=args[0]
-        ax.plot(1/w,p)
-        ax.set_xscale('log')
-        ax.axhline(y=cl95,color='r', linestyle='--')
-        ax.set_xlabel('Time Period (year)')
-        ax.set_ylabel('PSD (dB)')
-        ax.grid()
+    if len(kwargs)!=0:
+        for item,values in kwargs.items():
+            ax=values
+            ax.plot(1/f,p)
+            ax.set_xscale('log')
+            ax.axhline(y=cl95,color='r', linestyle='--')
+            ax.set_xlabel('Time Period (year)')
+            ax.set_ylabel('PSD (dB)')
+            ax.grid()
 
-    return p,w
+    return p,f,cl95
 #****************************** cross spectrum *************************##
 
-def xspec(x,y,sample_freq,*args):
-    fx = sc.fftpack.fft(x)
-    fy = sc.fftpack.fft(y)
-    fftfreq = np.fft.fftfreq(len(x),sample_freq) ### daily data it is 1./365 ## monthly data 1./12 ## yearly data=1
-    w = fftfreq[(fftfreq >= 0) |(fftfreq==-0.5)]
-    w[-1]=-1*w[-1]
-    Pxy= np.zeros((len(x),),dtype=complex)
-    Pxy = fx[(fftfreq >= 0) |(fftfreq==-0.5)]*np.conj(fy[(fftfreq >= 0) |(fftfreq==-0.5)])*(2/len(x))
-    if len(args)!=0:
-        ax=args[0]
-        i = fftfreq > 0
-        ax.plot(1/w,np.real(Pxy))
-        ax.set_xscale('log')
-        ax.set_xlabel('Time Period (year)')
-        ax.set_ylabel('PSD (dB)')
-        ax.grid()
+def xspec(x,y,sample_freq,window_type,**kwargs):
+    x=x-np.mean(x)
+    y=y-np.mean(y)
+    N=len(x)
+    dt=sample_freq
+    T=dt*N
+    fmax=1/(2*Dt);
+    if N != len(y): 
+        raise ValueError('Data sets x  and y of different lengths: %i , %i!'%(n, len(y)))
+        
+    if window_type==1: 
+        w1=np.hamming(N) 
+    elif window_type==2:
+        w1=np.hanning(N)
+    else:
+        w1=1
+  
+    fx = dt*sc.fftpack.fft(w1*x)
+    fy = dt*sc.fftpack.fft(w1*y)
+    fftfreq = np.fft.fftfreq(N,sample_freq)### daily data it is 1./365 ## monthly data 1./12 ## yearly data=1
+    i1=(fftfreq >= 0) |(fftfreq==-fmax)
+    f= fftfreq[i1]
+    f[-1]=-1*f[-1]
+    Pxy= np.zeros((len(f),),dtype=complex)
+    Pxy = fx[i1]*np.conj(fy[i1])*(2/T)
+    if len(kwargs)!=0: 
+        for item,values in kwargs.items():
+            ax=values
+            ax.plot(1/f,np.real(Pxy))
+            ax.set_xscale('log')
+            ax.set_xlabel('Time Period (year)')
+            ax.set_ylabel('Cospectra(dB)')
+            ax.grid()
 
-    return Pxy,w
+    return Pxy,f
 #****************************** coherence squared *************************##
-def spectra(*args):
+
+def spectra_pan(x,y,sample_freq,window_type,nwindow,alpha):
     #      f    - frequencies
     # %   Pxx  - power spectrum of x
     # %   Pyy  - power spectrum of y
@@ -78,79 +103,135 @@ def spectra(*args):
     # %   cxy  - cospectrum between x and y
     # %   qxy  - quadspectrum between x and y
     
-    nargs=len(args)
-    x = args[0]
     n = len(x)
-
-    #% generate power spectra
-    Pxx,w1 = spec(x,1,0)
-    Pxx=1/n*Pxx
-    f=w1*1
-    #% if one argument
-    if nargs == 1:
-        Pyy = []; Pxy = []; Cxy = []; C1 = []; C2=[];Phxy = []; cxy = []; qxy = []; Lh = [];
-        return f,Pxx,Pyy,Pxy,Cxy,C1,C2,Phxy,cxy,qxy
-    #% if two argument    
-    y = args[1]
-    
+    dt=sample_freq
     if n != len(y): 
         raise ValueError('Data sets x  and y of different lengths: %i , %i!'%(n, len(y)))
         f = []; Pxx = []; Pyy = []; Pxy = []; Cxy = []; C1 = [];C2=[]; Phxy = []; cxy = []; qxy = []; Lh = [];
         return f,Pxx,Pyy,Pxy,Cxy,C1,C2,Phxy,cxy,qxy
     
-    Pyy,w2     = spec(y,1,0)
-    Pxy,w3     = xspec(x,y,1)
-    
-    #% scale to have integral(Pxx) = variance(x)
    
+    n1 = 2**math.ceil(np.log2(len(x)))             ## number of points for fft
+    k=int(n1/nwindow)                              ## No of windows
+    
+    if window_type==1:
+        w1=np.hamming(k)
+    elif window_type==2:
+        w1=np.hanning(k)
+    else:
+        w1=1
+        
+    x1=np.zeros(k*nwindow)
+    y1=np.zeros(k*nwindow)
+    x1[0:len(x)]=x[:]
+    y1[0:len(y)]=y[:]
+
+    x1      = np.reshape(x1,(nwindow,k)).T
+    y1      = np.reshape(y1,(nwindow,k)).T
+    fftfreq = np.fft.fftfreq(k,dt)
+    nqf     = int(k/2+1)
+    ssx    = np.zeros(x1.shape,dtype=complex)
+    ssy    = np.zeros(x1.shape,dtype=complex)
+    ssxy   = np.zeros(x1.shape,dtype=complex)
+    for i in np.arange(nwindow):
+        ssx[:,i] = 2*(dt**2/k)*sc.fftpack.fft(w1*x1[:,i])*np.conj(sc.fftpack.fft(w1*x1[:,i]))
+        ssy[:,i] = 2*(dt**2/k)*sc.fftpack.fft(w1*y1[:,i])*np.conj(sc.fftpack.fft(w1*y1[:,i]))
+        ssxy[:,i] = 2*(dt**2/k)*sc.fftpack.fft(w1*x1[:,i])*np.conj(sc.fftpack.fft(w1*y1[:,i]))
+    
+    Pxx=1/k*np.mean(ssx[0:nqf,:],axis=1)
+    Pyy=1/k*np.mean(ssy[0:nqf,:],axis=1)
+    Pxy=1/k*np.mean(ssxy[0:nqf,:],axis=1)
+    
+    f=fftfreq[:nqf]
+    f[nqf-1]=-1*f[nqf-1]    
+    #% scale to have integral(Pxx) = variance(x)
+    Pxx=1/n*Pxx
     Pyy=1/n*Pyy
     Pxy=1/n*Pxy
     
     #% set co- and quad-spectra
     cxy = Pxy.real
     qxy = -Pxy.imag
-
+    #% generate phase spectra and make points where Cxy < C NaNs
+    Phxy = np.arctan2( qxy , cxy )*180/np.pi
+    #%ii = find( Cxy < C ); Phxy(ii) = NaN*ones(size(ii));
+    
     #% generate coherence and significance limit
-    Cxy = np.abs(Pxy)**2/ Pxx * Pyy;
-    df = 2*n #https://dsp.stackexchange.com/questions/16558/statistical-significance-of-coherence-values
-    alpha = 0.05
-    C1 = 1 - alpha**(2/(df-2));
+    Cxy = np.abs(Pxy)**2/ (Pxx * Pyy);
+    df = 2*nwindow #https://dsp.stackexchange.com/questions/16558/statistical-significance-of-coherence-values
+
+    C1 = 1 - alpha**(2/(df-2));   #     alpha = 0.05
    
     #% this is a very good approximation to:
     F = sc.stats.f.ppf(q=1-alpha, dfn=2, dfd=df-2)
     C2 = F/( 0.5*(df-2) + F);
 
-    #% generate phase spectra and make points where Cxy < C NaNs
-    Phxy = np.arctan2( qxy , cxy )*180/np.pi
-    #%ii = find( Cxy < C ); Phxy(ii) = NaN*ones(size(ii));
+    
     
     return f,Pxx,Pyy,Pxy,Cxy,C1,C2,Phxy,cxy,qxy
 
+
+
+#****************************** coherence square SCIPY and MATPLOTLIB ******************
+
+def spectra_scipy_matplotlib(x,y,sample_freq,window_type,nwindow,alpha):
+    #      f1,f2    - scipy and mlab frequencies
+    # %   Cxy1,Cxy2  - scipy and mlab coherence (not squared) between x and y
+    #        k -       nperseg/NFFT/each window length 
+    
+    n = len(x)
+    dt=sample_freq
+    if n != len(y): 
+        raise ValueError('Data sets x  and y of different lengths: %i , %i!'%(n, len(y)))
+        f = []; Pxx = []; Pyy = []; Pxy = []; Cxy = []; C1 = [];C2=[]; Phxy = []; cxy = []; qxy = []; Lh = [];
+        return f1,f2,Cxy1,Cxy2,C1,C2
+    
+   
+    n1 = 2**math.ceil(np.log2(len(x)))             ## number of points for fft
+    k=int(n1/nwindow)                              ## No of windows
+    
+    if window_type==1:
+        w1=np.hamming(k)
+        
+    elif window_type==2:
+        w1=np.hanning(k)
+        
+    else:
+        w1=1
+        
+    x=x-np.mean(x)
+    y=y-np.mean(y)
+    
+    f1, Cxy1 = signal.coherence(x,y,fs=dt,window=w1,nperseg=k,noverlap=0)
+    Cxy2,f2  = plt.mlab.cohere(x,y,window= w1, NFFT=k, noverlap=0, Fs=dt)
+    
+    #% generate coherence and significance limit
+    df = 2*nwindow #https://dsp.stackexchange.com/questions/16558/statistical-significance-of-coherence-values
+
+    C1 = 1 - alpha**(2/(df-2));   #     alpha = 0.05
+   
+    #% this is a very good approximation to:
+    F = sc.stats.f.ppf(q=1-alpha, dfn=2, dfd=df-2)
+    C2 = F/( 0.5*(df-2) + F);
+
+    
+    
+    return f1,f2,Cxy1,Cxy2,C1,C2,k
 #****************************** filter signal *************************##
 
 
-def filter_signal_scipy(signal,sample_freq,ltime_period,htime_period,opt_show_psd,keep_mean):
+def filter_signal_scipy(signal,sample_freq,ltime_period,htime_period,keep_mean):
     filter_signal=np.zeros(signal.shape)
     if any(np.isnan(signal)):
-        filter_signal=signal*1
+        raise ValueError('There is NaN in the signal')
     else:
         hf=1./ltime_period
         lf=1./htime_period
 
         temp_fft = sc.fftpack.fft(signal)
-        temp_psd = np.abs(temp_fft) ** 2
 
         fftfreq = np.fft.fftfreq(len(signal),sample_freq) ### daily data it is 1./365 ## monthly data 1./12 ## yearly data=1
-        i = fftfreq > 0
-
-        if opt_show_psd:
-            fig, ax = plt.subplots(1, 1, figsize=(8, 4))
-            ax.plot( (1/fftfreq[i]),temp_psd[i])
-            ax.set_xscale('log')
-            ax.set_xlabel('Time Period (year)')
-            ax.set_ylabel('PSD (dB)')
-            ax.grid()
-            
+          
         i1=(np.abs(fftfreq) >= lf) & (np.abs(fftfreq) <= hf)  
         inv_fft=np.zeros(temp_fft.size,dtype=complex)
         inv_fft[i1]=temp_fft[i1]
